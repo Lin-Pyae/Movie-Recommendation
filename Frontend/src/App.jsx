a789791275b0c7b8d81e20895d0b3f8074abc94f
Brag <brangtsawmaung89@gmail.com>
2024-03-11 23:38:22 +0630

    setup react and tailwind

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <>
+      <div className="w-full flex items-center justify-center text-2xl">Movie Recommendation System</div>
+    </>
+  )
+}
+
+export default App
